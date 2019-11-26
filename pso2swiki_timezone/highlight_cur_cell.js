385b03b96ef30634a2ad12c4dc64802104d83274
aalexx-S <alex530519@gmail.com>
2019-11-26 01:45:12 -0800

    Change current cell to red and next cell to orange
    Highlight current cell with red and next cell with orange.

--- pso2swiki_timezone/highlight_cur_cell.js
+++ pso2swiki_timezone/highlight_cur_cell.js
@@ -1,10 +1,10 @@
-// highlight next event cell
+// highlight current and next event cell
 // I am using string of int comparing to actual int
 function highlight_cur_cell(dd, hh) {
-	// remove current highlight cell
-	cell = document.getElementsByClassName('cur_highlight_cell');
-	for(let i = 0, c; c = cell[i]; i++) { // should only have one element
-		c.classList.remove('cur_highlight_cell');
+	// remove highlighted cell
+	cell = document.querySelectorAll('.cur_highlight_cell, .nxt_highlight_cell');
+	for(let i = 0, c; c = cell[i]; i++) { // should have either 0 or 2 objects
+		c.classList.remove('cur_highlight_cell', 'nxt_highlight_cell');
 	}
 
 	let ediv = document.getElementsByClassName('table-responsive')[0];
@@ -12,28 +12,40 @@ function highlight_cur_cell(dd, hh) {
 
 	// get correct row with date (dd)
 	let rid = 0;
-	for (let r; c = target_table.rows[rid].cells[0]; rid++)
-		if (c.innerText.substring(0, c.innerText.length - 1).startsWith(dd))
+	for (; c = target_table.rows[rid]; rid++)
+		if (c.cells[0].innerText.substring(0, c.cells[0].innerText.length - 1).startsWith(dd))
 			break;
+	if (rid >= target_table.rows.length)
+		return; // not match date found
 
 	// get correct column with jp hour (hh)
 	// use the last row in tHead instead of the second row since there may only be one row
 	let cid = 1;
 	let jp_row = target_table.tHead.rows[target_table.tHead.rows.length - 1];
 	for (let c; c = jp_row.cells[cid]; cid++)
-		if (c.innerText.substring(0, c.innerText.length - 1) > hh || cid >= jp_row.cells.length - 1)
+		if (cid >= jp_row.cells.length - 1 || c.innerText.substring(0, c.innerText.length - 1) > hh)
 			break;
-	if (cid >= jp_row.cells.length - 1)
-		cid = 1;
+	cid -= 1;
 
-	// highligh the cell
+	// highligh current cell
 	// check colspan because casino event
 	let cur_i = 0;
-	for (let i = 1, c; c = target_table.rows[rid].cells[i]; i++) {
+	let ind;
+	for (ind = 1, c; c = target_table.rows[rid].cells[ind]; ind++) {
 		cur_i += c.colSpan;
 		if (cur_i >= cid) {
 			c.classList.add('cur_highlight_cell');
 			break;
 		}
+	}
+	// highligh next cell
+	// check if change day
+	ind += 1;
+	if (ind == target_table.rows[rid].cells.length - 1) {
+		ind = 1;
+		rid += 1;
+	}
+	if (rid < target_table.rows.length) {
+		target_table.rows[rid].cells[ind].classList.add('nxt_highlight_cell');
 	}
 }
